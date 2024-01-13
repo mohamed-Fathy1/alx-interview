65998bf2d0e8cd944af6867df7cf66b896dda8a0
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-13 20:33:46 +0200

    Solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,20 +11,13 @@ def canUnlockAll(boxes):
        @boxes: list of lists
     '''
     isUnlockble = True
-    # list = reduce(lambda acc, arr: acc + arr, boxes)
-    # for i in range(1, len(boxes)):
-    #     print(i)
-    #     if i not in list:
-    #         isUnlockble = False
 
     res_dct = {i: False for i in range(len(boxes))}
     res_dct[0] = True
-    # print(list(res_dct.values()))
 
     loopAgain = True
     i = 0
     while (loopAgain and (not isUnlockble or i == 0)):
-        # print('loop again \n')
         keys = boxes[0]
         loopAgain = False
         for i in range(1, len(boxes)):
@@ -40,9 +33,6 @@ def canUnlockAll(boxes):
             keys += boxes[i]
             res_dct[i] = True
             loopAgain = True
-        # print("\n\n\n")
-
-    # print(res_dct)
 
     isUnlockble = all(flag is True for flag in list(res_dct.values()))
     return isUnlockble
