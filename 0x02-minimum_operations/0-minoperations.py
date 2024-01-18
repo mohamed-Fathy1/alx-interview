0341b4c58c26cd3b0067eb42b197940a05d204ab
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-18 18:22:19 +0200

    Add task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,37 +10,51 @@ def minOperations(n):
         Method that calculates the fewest number of operations
         needed to result in exactly n H characters in the file.
     """
-    strOp = 'H'
-    pasteStr = ''
-
-    def copyAll():
-        '''
-           returns the string copied
-        '''
-        return strOp
-
-    def paste():
-        '''
-            returns the string pasted
-        '''
-        return pasteStr
-
-    if n < 2:
+    if n <= 1:
         return 0
-    pasteStr = copyAll()
-    strOp += paste()
-    numOfOp = 2
 
-    while len(strOp) < n:
-        if len(strOp) % 2 == 0 or len(strOp) == n - 1:
-            strOp += paste()
-            numOfOp += 1
-        elif (len(strOp) * 2) < n:
-            pasteStr = copyAll()
-            strOp += paste()
-            numOfOp += 2
+    numOp = 0
+    i = 2
+
+    while i <= n:
+        if n % i == 0:
+            numOp += i
+            n = n / i
         else:
-            strOp += paste()
-            numOfOp += 1
+            i += 1
 
-    return numOfOp
+    return numOp
+    # strOp = 'H'
+    # pasteStr = ''
+    #
+    # def copyAll():
+    #     '''
+    #        returns the string copied
+    #     '''
+    #     return strOp
+    #
+    # def paste():
+    #     '''
+    #         returns the string pasted
+    #     '''
+    #     return pasteStr
+    #
+    # if n < 2:
+    #     return 0
+    # pasteStr = copyAll()
+    # strOp += paste()
+    # numOfOp = 2
+    #
+    # while len(strOp) < n:
+    #     if len(strOp) % 2 == 0 or len(strOp) == n - 1:
+    #         strOp += paste()
+    #         numOfOp += 1
+    #     elif (len(strOp) * 2) < n:
+    #         pasteStr = copyAll()
+    #         strOp += paste()
+    #         numOfOp += 2
+    #     else:
+    #         strOp += paste()
+    #         numOfOp += 1
+    #
+    # return numOfOp
