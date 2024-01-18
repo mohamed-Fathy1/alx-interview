150137a206f2b90bbfd6325666765caf19213801
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2024-01-18 18:09:35 +0200

    Add task 0

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -10,3 +10,7 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+
+n = 21654876512
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
